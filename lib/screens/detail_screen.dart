b6f820297c810749d0b0c1011c1064dd5fdbd810
Sekar Ayu <106504828+sekarayu99@users.noreply.github.com>
2024-11-07 14:46:03 +0700

    Update detail_screen.dart

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,37 +1,57 @@
 import 'package:flutter/material.dart';
 import 'package:digitalrecipe/model/recipe.dart';
 
-class DetailScreen extends StatelessWidget {
+class DetailScreen extends StatefulWidget {
   final Recipe recipe;
 
   DetailScreen({required this.recipe});
 
   @override
+  _DetailScreenState createState() => _DetailScreenState();
+}
+
+class _DetailScreenState extends State<DetailScreen> {
+  void toggleFavorite() {
+    setState(() {
+      widget.recipe.isFavorite = !widget.recipe.isFavorite;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(recipe.title),
+        title: Text(widget.recipe.title),
+        actions: [
+          IconButton(
+            icon: Icon(
+              widget.recipe.isFavorite ? Icons.favorite : Icons.favorite_border,
+              color: widget.recipe.isFavorite ? Colors.red : Colors.white,
+            ),
+            onPressed: toggleFavorite,
+          ),
+        ],
       ),
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Image.network(
-              recipe.imageUrl,
+              widget.recipe.imageUrl,
               height: 200,
               fit: BoxFit.cover,
             ),
             Padding(
               padding: const EdgeInsets.all(16.0),
               child: Text(
-                recipe.title,
+                widget.recipe.title,
                 style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
               ),
             ),
             Padding(
               padding: const EdgeInsets.all(16.0),
               child: Text(
-                recipe.description,
+                widget.recipe.description,
                 style: TextStyle(fontSize: 16),
               ),
             ),
@@ -41,3 +61,4 @@ class DetailScreen extends StatelessWidget {
     );
   }
 }
+
